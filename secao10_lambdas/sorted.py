dc432bf65703159e82044d38c90a0b6eff839464
alexandrelorena <alexandre.lorena@gmail.com>
2023-10-12 19:50:26 -0300

    atualização em Sorted

--- secao10_lambdas/sorted.py
+++ secao10_lambdas/sorted.py
@@ -7,8 +7,8 @@ Sorted
 - gera sempre uma lista,
 
 """
-
 # ------------------------------ ↓ Sorted com lista ↓ -------------------------------------
+
 print(f"\033[33m{'-' * 70}\n\033[94m{'↓ Sorted com lista ↓'.center(70)}\033[33m\n{'-' * 70}\033[0m")
 
 numeros = [6, 1, 8, 2]  # o sorted() gera sempre uma lista, indepente se a entrada for lista, tupla, etc
@@ -35,6 +35,7 @@ print(numeros)
 print(type(numeros))
 
 # ------------------------------ ↓ Sorted com tupla ↓ -------------------------------------
+
 print(f"\033[33m{'-' * 70}\n\033[94m{'↓ Sorted com set ↓'.center(70)}\033[33m\n{'-' * 70}\033[0m")
 
 numeros = {6, 1, 8, 2}  # o sorted() gera sempre uma lista, indepente se a entrada for lista, tupla, etc
@@ -48,6 +49,7 @@ print(numeros)
 print(type(numeros))
 
 # ------------------------------ ↓ Sorted com dicionário ↓ -------------------------------------
+
 print(f"\033[33m{'-' * 70}\n\033[94m{'↓ Sorted com dicionário ↓'.center(70)}\033[33m\n{'-' * 70}\033[0m")
 
 numeros = [6, 1, 8, 2]  # o sorted() gera sempre uma lista, indepente se a entrada for lista, tupla, etc
@@ -59,6 +61,7 @@ dicionario = {indice: valor for indice, valor in enumerate(numeros)}
 print(dicionario)
 print(type(dicionario))
 # ------------------------------ ↓ por chave ↓ -------------------------------------
+
 print(f"{'↓ por chave ↓'.center(70)}\n")
 print("print(f'Ordenando com sorted, por chave: {sorted(dicionario)}')\n")
 print(f'Ordenando com sorted, por chave: {sorted(dicionario)}\n')
@@ -66,10 +69,12 @@ print(f'Ordenando com sorted, por chave: {sorted(dicionario)}\n')
 print(numeros)
 print(type(numeros))
 # ------------------------------ ↓ por valor ↓ -------------------------------------
+
 print(f"{'↓ por valor ↓'.center(70)}\n")
 print("dicionario_ordenado = dict(sorted(dicionario.items(), key=lambda item: item[1]))\n")
 dicionario_ordenado = dict(sorted(dicionario.items(), key=lambda item: item[1]))
 
 print(f'Ordenando com sorted, por valor: {dicionario_ordenado}')
+
 # ------------------------------ ↓ por valor ↓ -------------------------------------
 
