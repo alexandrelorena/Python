0b69e6aba2b62e882682368cb459c43a2e9f8edc
alexandrelorena <alexandre.lorena@gmail.com>
2023-10-14 01:36:58 -0300

    atualização

--- secao10_lambdas/min-e-max.py
+++ secao10_lambdas/min-e-max.py
@@ -6,53 +6,56 @@ max() -> Retorna o maior valor em um iterável ou o maior de dois ou mais elemen
 """
 # ------------------------------ ↓ Min e Max ↓ -------------------------------------
 
-print(f"\033[33m{'-' * 70}\n\033[94m{'↓ Min e Max ↓'.center(70)}\033[33m\n{'-' * 70}\033[0m")
+print(f"\033[33m{'-' * 70}\n\033[94m{' Min e Max '.center(70)}\033[33m\n{'-' * 70}\033[0m")
 
 # ------------------------- ↓ lista ↓ ----------------------------
 
 print(f"\033[33m{'-' * 70}\n\033[94m{'↓ lista ↓'.center(70)}\033[33m\n{'-' * 70}\033[0m")
 
 lista = [1, 8, 4, 99, 34, 129]
-print(f"{'↓ max() ↓'.center(70)}\n")
-print(max(lista))
-print(f"{'↓ min() ↓'.center(70)}\n")
-print(min(lista))
-print(f"{'↓ tipo ↓'.center(70)}\n")
-print(type(lista))
+
+# ------------------------- ↓ max ↓ ----------------------------
+
+print((f"\033[31mlista = {lista} | "+ f"\033[36mTipo: {type(lista)}\n\033[0m"))
+
+print(f"\033[31mmax() >> \033[36m{max(lista)} \n\033[0m")
+
+# ------------------------- ↓ min ↓ ----------------------------
+
+print(f"\033[31mmin() >> \033[36m{min(lista)} \033[0m")
 
 # ------------------------- ↓ tupla ↓ ----------------------------
 
 print(f"\033[33m{'-' * 70}\n\033[94m{'↓ tupla ↓'.center(70)}\033[33m\n{'-' * 70}\033[0m")
 
 tupla = (1, 8, 4, 99, 34, 129)
-print(f"{'↓ max() ↓'.center(70)}\n")
-print(max(tupla))
-print(f"{'↓ min() ↓'.center(70)}\n")
-print(min(tupla))
-print(f"{'↓ tipo ↓'.center(70)}\n")
-print(type(tupla))
+
+# ------------------------- ↓ max ↓ ----------------------------
+
+print((f"\033[31mtupla = {tupla} | "+ f"\033[36mTipo: {type(tupla)}\n\033[0m"))
+
+print(f"\033[31mmax() >> \033[36m{max(tupla)} \n\033[0m")
+
+# ------------------------- ↓ min ↓ ----------------------------
+
+print(f"\033[31mmin() >> \033[36m{min(tupla)} \033[0m")
+
+# ---------------------------------------------------------------
 
 # ------------------------- ↓ conjunto ↓ ----------------------------
 
 print(f"\033[33m{'-' * 70}\n\033[94m{'↓ conjunto ↓'.center(70)}\033[33m\n{'-' * 70}\033[0m")
 
 conjunto = {1, 8, 4, 99, 34, 129}
-# ------------------------- ↓ max ↓
-print((f"conjunto = {conjunto} | " + f"\033[36mTipo: {type(conjunto)}\n\033[0m").center(70))
-print(f"{f' max() >> {max(conjunto)} '.center(70)}\n")
-# ------------------------- ↓ min ↓ ----------------------------\033[35m:
-print(f"{f' min() >> {min(conjunto)} '.center(70)}\n")
-# ------------------------- ↓ tipo ↓ ----------------------------
-print(f"{'↓ tipo ↓'.center(70)}\n")
-print(f"{f' {type(conjunto)} '.center(70)}\n")
 
+# ------------------------- ↓ max ↓ ----------------------------
+
+print((f"\033[31mconjunto = {conjunto} | "+ f"\033[36mTipo: {type(conjunto)}\n\033[0m"))
+
+print(f"\033[31mmax() >> \033[36m{max(conjunto)} \n\033[0m")
+
+# ------------------------- ↓ min ↓ ----------------------------
+
+print(f"\033[31mmin() >> \033[36m{min(conjunto)} \033[0m")
 
 # ---------------------------------------------------------------
-
-conjunto = {1, 8, 4, 99, 34, 129}
-print(f"{'↓ max() ↓'.center(70)}\n")
-print(max(conjunto))
-print(f"{'↓ min() ↓'.center(70)}\n")
-print(min(conjunto))
-print(f"{'↓ tipo ↓'.center(70)}\n")
-print(type(conjunto))
