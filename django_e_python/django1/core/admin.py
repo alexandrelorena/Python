0f181d4ce519e16a8a12bd305861ef71be8c7ecf
alexandrelorena <alexandre.lorena@gmail.com>
2023-12-18 17:37:47 -0300

    atualização Django admin

--- django_e_python/django1/core/admin.py
+++ django_e_python/django1/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Produto, Cliente
+
+admin.site.register(Produto)
+admin.site.register(Cliente)
+
--- django_e_python/django1/core/models.py
+++ django_e_python/django1/core/models.py
@@ -6,6 +6,8 @@ class Produto(models.Model):
     preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     estoque = models.IntegerField('Quantidade emEstoque')
 
+    def __str__(self):
+        return f'{self.nome} | {self.estoque}'
 
 class Cliente(models.Model):
     nome = models.CharField('Nome', max_length=100)
