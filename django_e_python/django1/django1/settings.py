c95de819a71e2c3977d9e5aac5d20a87a0d94186
alexandrelorena <alexandre.lorena@gmail.com>
2023-12-20 15:34:52 -0300

    atualização

--- django_e_python/django1/django1/settings.py
+++ django_e_python/django1/django1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x*ar=y0zu!g!^acgm3q6ap2xh)!akmpi@d1eg731($gg0jp6=c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
