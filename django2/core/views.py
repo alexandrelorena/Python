5375a75030fbc0952799af51081280287d2d6b32
ALEXANDRE LORENA <alexandre.lorena@gmail.com>
2023-12-28 07:55:11 -0300

    atualização: Django2 >> Django com MySQL

--- django2/core/views.py
+++ django2/core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'core/index.html')
+
+def contato(request):
+    return render(request, 'core/contato.html')
+
+def produto(request):
+    return render(request, 'core/produtos.html')
